5862bcdec04f3672d6c86f3bd1917e02b5a0c92d
Fergus MacDermot <fergusmacdermot@gmail.com>
2019-12-07 09:54:01 +0800

    Adding provider and repo files

---
+++ repository.tf
@@ -0,0 +1,15 @@
+# the terraform resource for the repository
+resource "github_repository" "github_management" {
+  name        = "github-management"
+  description = "Terraform based repository to manage all our GutHub repositories"
+
+  private            = false
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  auto_init          = false
+  license_template   = "mit"
+  topics             = ["config", "terraform"]
+}
