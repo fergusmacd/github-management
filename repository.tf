2ae0b731f6029d1aebc276f5e1eb57786e5ba045
Fergus MacDermot <fergusmacdermot@gmail.com>
2019-12-07 10:59:53 +0800

    Adding branch protection

--- repository.tf
+++ repository.tf
@@ -1,7 +1,7 @@
 # the terraform resource for the repository
 resource "github_repository" "github_management" {
   name        = "github-management"
-  description = "Terraform based repository to manage all our GutHub repositories"
+  description = "Terraform based repository to manage all our GitHub repositories"
 
   private            = false
   has_issues         = true
@@ -11,4 +11,16 @@ resource "github_repository" "github_management" {
   allow_rebase_merge = true
   auto_init          = false
   topics             = ["config", "terraform"]
+}
+
+# Protect the master branch of the repository.
+resource "github_branch_protection" "team_baseline_config" {
+  repository     = "${github_repository.github_management.name}"
+  branch         = "master"
+
+  required_status_checks {
+    strict = false
+    contexts = ["atlas/mononoke/github-management", ]
+  }
+
 }
